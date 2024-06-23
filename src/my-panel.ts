ada63fb3adbee99a0198c2e98d06b097d51d93fe
Deepak Surya <ideepaksuryas@gmail.com>
2024-06-23 20:11:19 +0530

    init: lit with same component

---
+++ src/my-panel.ts
@@ -0,0 +1,39 @@
+import { html, css, LitElement } from "lit";
+import { customElement, property } from "lit/decorators.js";
+
+@customElement("my-panel")
+export class MyPanel extends LitElement {
+  @property({ type: String })
+  title = "WIDGET";
+  static styles = css`
+    .container {
+      min-width: 20rem;
+      min-height: 5rem;
+    }
+    .title {
+      background-color: var(--my-panel-primary-bg, red);
+      color: var(--my-panel-primary-color, black);
+      padding: 0.8rem;
+      border-top-left-radius: 1rem;
+      border-top-right-radius: 1rem;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+    }
+    .body {
+      padding: 1rem;
+      border: 1px solid var(--my-panel-primary-bg, red);
+    }
+  `;
+  render() {
+    return html`
+      <div class="container">
+        <div class="title">
+          ${this.title}
+          <div>😁</div>
+        </div>
+        <div class="body">CONTENT</div>
+      </div>
+    `;
+  }
+}
